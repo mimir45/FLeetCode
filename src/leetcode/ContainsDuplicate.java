546c243e724f2ff5092221234b45229b715bcb97
Samir <sa.alizada@andersenlab.com>
2025-11-12 01:00:07 +0400

    s

--- src/leetcode/ContainsDuplicate.java
+++ src/leetcode/ContainsDuplicate.java
@@ -1,20 +1,29 @@
 package leetcode;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContainsDuplicate {
-    public static void main(String[] args) {
-
+  public boolean containsDuplicate(int[] nums) {
+      Set<Integer> set = new HashSet<>();
+    for(int i : nums) {
+      if(set.contains(i)) {
+        return true;
+      }
+      set.add(i);
     }
-    public boolean containsDuplicate(int[] nums) {
-        HashMap hs = new HashMap();
-        for(int i:nums){
-            if(hs.containsKey(i)){
-                return true;
-            }
-            hs.put(i,1);
-        }
-        return false;
+    return false;
+  }
 
+  public boolean containsDuplicate2(int[] nums) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for(int i : nums) {
+      if(map.containsKey(i)) {
+        return true;
+      }
+      map.put(i, 1);
     }
+    return false;
+  }
 }
--- src/leetcode/ValidAnagram.java
+++ src/leetcode/ValidAnagram.java
@@ -1,38 +1,46 @@
 package leetcode;
 
-import java.util.HashMap;
-
 public class ValidAnagram {
-    public static void main(String[] args) {
 
-
-    boolean result= isAnagram("anagram", "nagaram");
-    System.out.println(result);
-
+  public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) {
+      return false;
     }
-    public static boolean isAnagram(String s, String t) {
-       HashMap<Character,Integer> map = new HashMap<>();
-       if(s.length()!=t.length()){
-           return false;
-       }
-       for(char c:s.toCharArray()){
-           if(map.containsKey(c)){
-               map.put(c,map.get(c)+1);
-           }else{
-               map.put(c,1);
-           }
-       }
-       for(char c:t.toCharArray()){
-           if(map.containsKey(c)){
-               if(map.get(c)>0){
-                   map.put(c,map.get(c)-1);
-               } else{
-                   return false;
-               }
-           }else{
-               return false;
-           }
-       }
-       return true;
+
+//    // cahracter and count for example  anagram a repeted 3 times
+//    HashMap<Character, Integer> map = new HashMap<>();
+//
+//    for (Character c : s.toCharArray()) {
+//      if (map.containsKey(c)) {
+//        map.put(c, map.get(c) + 1);
+//      } else {
+//        map.put(c, 1);
+//      }
+//    }
+//    for (Character c : t.toCharArray()) {
+//      if (map.containsKey(c)) {
+//        if (map.get(c) > 0) {
+//          map.put(c, map.get(c) - 1);
+//        } else {
+//          return false;
+//        }
+//      } else {
+//        return false;
+//      }
+//    }
+
+    int[] chars1 = new int[26];
+    int[] chars2 = new int[26];
+
+    for (int i = 0; i < s.length(); i++) {
+      chars1[s.charAt(i) - 'a']++;
+      chars2[t.charAt(i) - 'a']++;
     }
+    for (int i = 0; i < 26; i++) {
+      if (chars1[i] != chars2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
